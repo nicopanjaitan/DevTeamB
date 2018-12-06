0d587c2ce497ee1f5ac57e6798b647f26ac0b052
Nani Hutagaol <nanihutagaol@gmail.com>
2018-12-06 15:22:11 +0700

    upd simple fe inv
    update the simple FE of inventory

--- fe_inv_mgt/js/main.js
+++ fe_inv_mgt/js/main.js
@@ -4,3 +4,22 @@ $(function(){
 	$("#content").load("template/content.html");
 	$("#footer").load("template/footer.html"); 
 });
+
+//convert form data to json
+function formToJson( form ) {
+	var obj = {};
+	var elements = form.querySelectorAll( "input, select, textarea" );
+
+	for( var i = 0; i < elements.length; ++i ) {
+	    var element = elements[i];
+	    var key = element.name;
+	    var value = element.value;
+
+	    if( key ) {
+	        obj[key] = value;
+	    }
+	}
+
+	return JSON.stringify( obj );
+}
+
